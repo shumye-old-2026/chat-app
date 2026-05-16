17bf3ebee985e19df4f627f330d2bdb931bb7354
ashumyetadesse-web <shumyetadesseasefa5@gmail.com>
2026-05-16 17:43:34 +0300

    add updated firebase config

--- firebase_config.js
+++ firebase_config.js
@@ -2,13 +2,13 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebas
 import { getDatabase } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-database.js";
 
 const firebaseConfig = {
-    apiKey: "YOUR_API_KEY",
-    authDomain: "YOUR_AUTH_DOMAIN",
-    databaseURL: "YOUR_DATABASE_URL",
-    projectId: "YOUR_PROJECT_ID",
-    storageBucket: "YOUR_STORAGE_BUCKET",
-    messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-    appId: "YOUR_APP_ID"
+  apiKey: "የአንተ-እውነተኛ-API-KEY-እዚህ-ይግባ",
+  authDomain: "የአንተ-ፕሮጀክት-ስም.firebaseapp.com",
+  databaseURL: "https://የአንተ-ፕሮጀክት-ስም-default-rtdb.firebaseio.com",
+  projectId: "የአንተ-ፕሮጀክት-ስም",
+  storageBucket: "የአንተ-ፕሮጀክት-ስም.appspot.com",
+  messagingSenderId: "የአንተ-SENDER-ID",
+  appId: "የአንተ-APP-ID"
 };
 
 const app = initializeApp(firebaseConfig);
